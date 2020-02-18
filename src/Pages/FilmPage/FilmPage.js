53eb28a5c902cc0011bb096ce8555fe993f20e76
seachai <imchai@gmail.com>
2020-02-18 09:25:53 -0500

    Added the movie fetch AP

--- src/Pages/FilmPage/FilmPage.js
+++ src/Pages/FilmPage/FilmPage.js
@@ -16,25 +16,25 @@ const FilmPage = (id) => {
 
   useEffect(() => {
     try {
-      // fetchTMDB();
+      fetchTMDB();
     } catch (error) {
       console.log(error.message);
     }
   }, []);
 
-  // const fetchTMDB = async () => {
-  //   const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}`;
-  //   const apiKey = "ec7cf9725335473ff9bc286b6f5045a5";
-  //   const response = await axios({
-  //     url: apiUrl,
-  //     method: "GET",
-  //     responseType: "json",
-  //     params: {
-  //       api_key: apiKey
-  //     }
-  //   });
-  //   setMovie(response.data);
-  // };
+  const fetchTMDB = async () => {
+    const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}`;
+    const apiKey = "ec7cf9725335473ff9bc286b6f5045a5";
+    const response = await axios({
+      url: apiUrl,
+      method: "GET",
+      responseType: "json",
+      params: {
+        api_key: apiKey
+      }
+    });
+    setMovie(response.data);
+  };
 
   return (
     <>
