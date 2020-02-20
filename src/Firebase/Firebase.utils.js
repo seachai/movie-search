0d478ebe04dfdfe0e65e6dbba2c376782c2f7121
Chai Lee <imchai@gmail.com>
2020-02-20 09:40:00 -0500

    Changed env stuff

--- src/Firebase/Firebase.utils.js
+++ src/Firebase/Firebase.utils.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5YdY_PuUJh2izV0R93qUOKkM1Mghm3sc",
-  authDomain: "cinemachai-18ad4.firebaseapp.com",
-  databaseURL: "https://cinemachai-18ad4.firebaseio.com",
-  projectId: "cinemachai-18ad4",
-  storageBucket: "cinemachai-18ad4.appspot.com",
-  messagingSenderId: "195461152279",
-  appId: "1:195461152279:web:b188dc8aa55fa0b6c1a652"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
